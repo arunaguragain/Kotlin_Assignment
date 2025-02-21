4354fb361f6c71e53df5517174905a7ed98444e9
Unika1 <guragainaruna@gmail.com>
2025-02-21 09:14:17 +0545

    Model of review done

---
+++ app/src/main/java/com/example/newproject/model/ReviewModel.kt
@@ -0,0 +1,40 @@
+package com.example.newproject.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class ReviewModel(
+    val id: String = " ",
+    val email: String = "",
+    val rating: Int = 0,
+    val message: String = ""
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readInt(),
+        parcel.readString() ?: ""
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(email)
+        parcel.writeInt(rating)
+        parcel.writeString(message)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ReviewModel> {
+        override fun createFromParcel(parcel: Parcel): ReviewModel {
+            return ReviewModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ReviewModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/newproject/repository/ReviewRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.example.newproject.repository
+
+class ReviewRepositoryImpl {
+}
